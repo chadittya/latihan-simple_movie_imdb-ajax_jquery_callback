df9162af25f1afeb8c089fe2548a7ec4129aa234
chadittya <chadittya@gmail.com>
2022-11-22 11:56:56 +0700

    mengkomentasi html tag dibawah class = modal-body karena sudah tidak digunakan. mengubah penggunaan ajax menjadi fetch untuk memanage API untuk latihan penggunaannya

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
-$(".search-button").on("click", function () {
-  $.ajax({
-    url:
-      "http://www.omdbapi.com/?i=tt3896198&apikey=7506a05e&s=" +
-      $(".input-keyword").val(),
-    success: (results) => {
-      const movies = results.Search;
+// MENGGUNAKAN JQUERY AJAX
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url:
+//       "http://www.omdbapi.com/?i=tt3896198&apikey=7506a05e&s=" +
+//       $(".input-keyword").val(),
+//     success: (results) => {
+//       const movies = results.Search;
+//       movies.forEach((m) => {
+//         $(showCards(m)).appendTo(".movie-container");
+//       });
+
+//       // ketika tombol detail di click
+//       $(".modal-detail-button").on("click", function () {
+//         //   console.log($(this).attr("data-imdbid")); //cek attribut data-imdbid
+//         $.ajax({
+//           url:
+//             "http://www.omdbapi.com/?apikey=7506a05e&i=" +
+//             $(this).attr("data-imdbid"),
+//           success: (m) => {
+//             const movieDetail = showModals(m);
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+// MENGGUNAKAN FETCH
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", function () {
+  const inputKeyword = document.querySelector(".input-keyword");
+  fetch(
+    "http://www.omdbapi.com/?i=tt3896198&apikey=7506a05e&s=" +
+      inputKeyword.value
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      const movies = r.Search;
+      let cards = "";
       movies.forEach((m) => {
-        $(showCards(m)).appendTo(".movie-container");
+        cards += showCards(m);
       });
+      const movieContainer = document.querySelector(".movie-container");
+      movieContainer.innerHTML = cards;
 
       // ketika tombol detail di click
-      $(".modal-detail-button").on("click", function () {
-        //   console.log($(this).attr("data-imdb-id")); //cek attribut data-imdb-id
-        $.ajax({
-          url:
-            "http://www.omdbapi.com/?apikey=7506a05e&i=" +
-            $(this).attr("data-imdb-id"),
-          success: (m) => {
-            const movieDetail = showModals(m);
-            $(".modal-body").html(movieDetail);
-          },
-          error: (e) => {
-            console.log(e.responseText);
-          },
+      const detailsButton = document.querySelectorAll(".modal-detail-button");
+      detailsButton.forEach((db) => {
+        db.addEventListener("click", function () {
+          // console.log(this.dataset.imdbid); //cek dapat no imdb
+          const imdbid = this.dataset.imdbid;
+          fetch("http://www.omdbapi.com/?apikey=7506a05e&i=" + imdbid)
+            .then((r) => r.json())
+            .then((m) => {
+              const movieDetail = showModals(m);
+              const modalBody = document.querySelector(".modal-body");
+              modalBody.innerHTML = movieDetail;
+            });
         });
       });
-    },
-    error: (e) => {
-      console.log(e.responseText);
-    },
-  });
+    });
 });
 
 function showCards(m) {
@@ -41,7 +78,7 @@ function showCards(m) {
           <h5 class="card-title">${m.Title}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
           <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
-          data-bs-target="#movie-detail-modal" data-imdb-id="${m.imdbID}">Details</a>
+          data-bs-target="#movie-detail-modal" data-imdbid="${m.imdbID}">Details</a>
         </div>
       </div>
     </div>
